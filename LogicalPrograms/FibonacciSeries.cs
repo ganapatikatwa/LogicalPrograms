2df828b1ac2a0c5ac6acfc1bb9d3e6dd37e7cf4a
Ganapati <ganapatasakatwa@gmail.com>
2023-05-28 11:28:18 +0530

    [GK] Fibonacci Series

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class FibonacciSeries
+    {
+        public void FSeries()
+        {
+            int n1 = 0;
+            int n2 = 1;
+            int i;
+            int n3=0;
+            int count;
+            Console.WriteLine("Enter The Number of Count Required for Fibonacci Series");
+            count= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(n1+" "+n2+" ");
+            for (i=2;i<count;i++)
+            {
+                n3 = n1 + n2;
+                Console.WriteLine(n3+" ");
+                n1 = n2;
+                n2 = n3;
+            }
+            Console.WriteLine("Fibonacci Final Count is--"+n3);
+        }
+    }
+}
---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,27 @@
+﻿using LogicalPrograms;
+using System;
+namespace AddressBookProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Programs");
+            while (true)
+            {
+
+                Console.WriteLine("Enter The Choice \n1)Fibonacci Serries");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        FibonacciSeries fibonacciSeries=new FibonacciSeries();
+                        fibonacciSeries.FSeries();
+                        break;
+
+                }
+            }
+        }
+    }
+}
+
